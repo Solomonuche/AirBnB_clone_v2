480d12139ac69ec289639f68a99884cb07d658e6
Solomon <solomonuche@gmail.com>
2024-01-25 21:23:53 +0100

    C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" a script that starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_pyfile("__init__.py")
+
+
+@app.route("/", strict_slashes=False)
+def display():
+    """ Display Hello HBNB!"""
+
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Display Hello HBNB!"""
+
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def var_C(text):
+    """ Display Hello HBNB!"""
+
+    if "_" in text:
+        text = text.replace("_", " ")
+
+    return f"C {text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
