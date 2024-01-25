aa2aa3a3273ee3f77ced46f10e96f50c78a95e5a
Solomon <solomonuche@gmail.com>
2024-01-25 21:24:41 +0100

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,11 +13,13 @@ def display():
 
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def display1():
     """ Display Hello HBNB!"""
 
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
