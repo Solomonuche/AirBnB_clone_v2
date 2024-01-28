8520dc8465212a40a10b3718bfc90c4675102700
Solomon <solomonuche@gmail.com>
2024-01-28 17:24:48 +0100

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception=None):
+    """ Perform clean up after request"""
+
+    storage.close()
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """ render list states"""
+
+    obj = storage.all(State)
+    states = []
+    for values in obj.values():
+        states.append(values)
+    
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
