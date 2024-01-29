fc5087ca04ad62f9b1b83b4d8f3d3e8ef7a9ecc1
Solomon <solomonuche@gmail.com>
2024-01-29 03:37:35 +0100

    added cities getter

--- models/state.py
+++ models/state.py
@@ -3,11 +3,28 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+import models
+from datetime import datetime
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
+
+    ""
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    cities = relationship("City", back_populates="state")
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship("City", back_populates="state")
+    else:
+        @property
+        def cities(self):
+            """returns a list of all cities from current state"""
+            cities_list = []
+            city_dict = models.storage.all(City)
+            for key, value in city_dict.items():
+                if value.state_id == self.id:
+                    cities_list.append(value)
+            return cities_list
